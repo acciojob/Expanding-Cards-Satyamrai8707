40f84a418437d06345a5727a73b644caaff44c54
Satyam Rai <114270778+Satyamrai8707@users.noreply.github.com>
2025-10-07 14:47:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-//your JS code here. If required.
-<script>
-      const panels = document.querySelectorAll(".panel");
+const panels = document.querySelectorAll('.panel');
 
-      panels.forEach((panel) => {
-        panel.addEventListener("click", () => {
-          removeActiveClasses();
-          panel.classList.add("active");
-        });
-      });
+panels.forEach(panel => {
+  panel.addEventListener('click', () => {
+    removeActiveClasses();
+    panel.classList.add('active');
+  });
+});
 
-      function removeActiveClasses() {
-        panels.forEach((panel) => {
-          panel.classList.remove("active");
-        });
-      }
-    </script>
+function removeActiveClasses() {
+  panels.forEach(panel => {
+    panel.classList.remove('active');
+  });
+}
